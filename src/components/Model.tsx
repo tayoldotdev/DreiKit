fa04269fbeeca44bbe34046a62e1c49481dc0d34
pwnker <tilen.okretic@gmail.com>
2024-02-04 16:20:25 +0100

    feature: Defined grapher and postprocessor types

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -3,24 +3,43 @@ import { useGLTF } from '@react-three/drei';
 
 import type { Object3D, Object3DEventMap } from 'three';
 
-type ModelProps = {
+
+/**
+ *  Indexed 3D model elements state managmnet {simmilar to useState} 
+ *  @returns [graph, setGraph]
+ */
+export type Grapher = [
+    Object3D<Object3DEventMap>[], // graph
+    (obj: Object3D<Object3DEventMap>[]) => void, // setGraph
+];
+
+/**
+ * Function that executes before the 3D model is indexed
+ * @param {Function} function that can be used to rename 3D model elements  
+ * @param {Object}   3D element object reference 
+ */
+export type Preprocessor = (upadteKey: (oldKey: string, newKey: string) => boolean, element: Object3D<Object3DEventMap>) => void; 
+
+/**
+ * @type {Object} ModelProps
+ */
+interface ModelProps {
     url: string;
-    grapher: [
-        Object3D<Object3DEventMap>[],
-        (o: Object3D<Object3DEventMap>[]) => void,
-    ];
-    preprocessor?: (
-        updateKey: (okey: string, nkey: string) => boolean,
-        elm: Object3D<Object3DEventMap>,
-    ) => void;
+    grapher: Grapher,
+    preprocessor?: Preprocessor,
 };
 
-const Model = ({ url, grapher: [, setGraph], preprocessor }: ModelProps) => {
+/**
+ * JSX component that can be used to put a 3D model on a  webpage
+ */
+function Model({ url, grapher: [, setGraph], preprocessor }: ModelProps) {
     const gltf = useGLTF(url, true, true);
 
     useEffect(() => {
         const graph =
-            gltf.scene.children[0]?.children[0]?.children[0]?.children;
+            gltf.scene.children[0]?.children[0]?.children[0]?.children[0]?.children;
+
+        console.log(graph)
 
         if (graph === undefined) {
             throw new Error(
